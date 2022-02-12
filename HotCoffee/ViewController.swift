5bc6b74750f28cdce91993e1daaf3d0a6ff2dfb8
Erluan Felix Batista <erluan@gmail.com>
2022-02-12 17:03:54 -0300

    Initial Commit

---
+++ HotCoffee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotCoffee
+//
+//  Created by Erluan Felix Batista on 12/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
