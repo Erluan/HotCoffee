2a84684ca68e90c281d94c7e48c8ff908e5e94f2
Erluan Felix Batista <erluan@gmail.com>
2022-02-16 20:07:21 -0300

    :recycle: updating files

--- HotCoffee/Controllers/OrdersTableViewController.swift
+++ HotCoffee/Controllers/OrdersTableViewController.swift
@@ -10,4 +10,29 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populateOrders()
+    }
+    
+    private func populateOrders(){
+        
+        guard let coffeeOrdersUrl = URL(string: "https://island-bramble.glitch.me/orders") else {
+            fatalError("URL was incorrect")
+        }
+        
+        let resource = Resource<[Order]>(url: coffeeOrdersUrl)
+        
+        WebService().load(resource: resource){ result in
+            
+            switch result {
+                case .success(let orders):
+                    print(orders)
+                case .failure(let error):
+                    print(error)
+            }
+        }
+    }
+    
+
 }
